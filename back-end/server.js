476ed94b4fdc353139f7438ba64eef66e2b72d18
Claudiu <claudiu.harton@gmail.com>
2019-11-24 01:32:40 +0200

    added websocket

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes");
+const cors = require("cors");
 let port = 8080;
 
 const app = express();
-app.use(bodyParser.json());
 
-app.use("/api", router);
+const configure = app => {
+  app.use(cors());
+  app.use(bodyParser.json());
+  // app.use(session);
 
-app.listen(port, () => {
-  console.log("Server is running on: " + port);
+  app.use("/api", router);
+  // app.use("/", express.static("../student-planner-front/dist"));
+};
+
+configure(app);
+
+let WSServer = require("ws").Server;
+let server = require("http").createServer();
+
+// Create web socket server on top of a regular http server
+let wss = new WSServer({
+  server: server
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "API Ready" });
+// Also mount the app here
+server.on("request", app);
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log(`received: ${message}`);
+
+    ws.send(
+      JSON.stringify({
+        answer: 42
+      })
+    );
+  });
+});
+
+server.listen(port, function() {
+  console.log(`http/ws server listening on ${port}`);
 });
