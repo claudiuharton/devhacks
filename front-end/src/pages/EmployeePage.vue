39fcc259f95803aea09ec7c61e26f82a25034369
Cezin <cezu.98@gmail.com>
2019-11-23 21:47:17 +0200

    Routes

---
+++ front-end/src/pages/EmployeePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn round icon="check" class="bg-green text-grey-1 q-ma-xl"></q-btn>
+    <!-- <q-btn
+      align="around"
+      class="btn-fixed-width"
+      color="brown-5"
+      label="Login"
+      icon="exit_to_app"
+      @click="$router.push('/display')"
+    />-->
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "LoginPage"
+};
+</script>
